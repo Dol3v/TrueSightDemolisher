3a4cfeba7a31e8b71c93444a074f8ba1c35b8395
Dolev <dol12dab@gmail.com>
2023-11-30 02:19:48 +0200

    Refactored Main.cpp

--- TrueSightDemolisher/Offsets.h
+++ TrueSightDemolisher/Offsets.h
@@ -7,6 +7,32 @@
 //
 
 //
+// Offsets from start of EPROCESS
+//
+
+constexpr ULONG c_TokenOffset = 0x4b8;
+constexpr ULONG c_ProcessIdOffset = 0x440;
+constexpr ULONG c_ProcessLinksOffset = 0x448;
+
+//
+// Offset of system process pointer from kernel start
+//
+constexpr ULONG c_PsInitialSystemProcess = 0xcfc420;
+
+//
+// Token privileges (i.e SEP_PROCESS_PRIVILEGES), shortened to exclude EnabledByDefault
+//
+struct ShortenedPrivileges
+{
+	ULONGLONG Present;
+	ULONGLONG Enabled;
+	// ULONGLONG EnabledByDefault;
+};
+
+// offset of SEP_TOKEN_PRIVILEGES from TOKEN
+constexpr ULONG c_PrivilegesOffset = 0x40;
+
+//
 // Return the offset of UniqueProcessId from the start of EPROCESS
 //
 UINT GetProcessIdOffsetFromProcess(IKernelReadWrite* Rw);
